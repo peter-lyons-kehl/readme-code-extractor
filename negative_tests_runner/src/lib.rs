f31541e1fe21cf094cbfbea3f458b81041c51ef5
Peter Lyons Kehl <peter.kehl@gmail.com>
2026-04-26 00:30:23 -0700

    negative_tests work

--- negative_tests_runner/src/lib.rs
+++ negative_tests_runner/src/lib.rs
@@ -10,14 +10,25 @@ mod tests {
         // enforced by ../negative_tests/src/lib.rs
         let cmd = Command::new("cargo").arg("build");
         let cmd = cmd.current_dir(std::fs::canonicalize("../negative_tests")?);
-        
+
         let assert = cmd.assert().failure();
+
+        assert.stderr_eq(
+            "...\n\
+             [..]ERROR[no-panic]: detected panic in function `main`[..]\n\
+             ...\n\
+             [..]ERROR[no-panic]: detected panic in function `divide`[..]\n\
+             ...\n\
+             [..]ERROR[no-panic]: detected panic in function `slice_access`[..]\n\
+             ...\n",
+        );
+        /*
         let output = assert.get_output();
         let stderr: &str = str::from_utf8( &output.stderr )?;
 
         assert!( stderr.contains("ERROR[no-panic]: detected panic in function `divide`"));
         assert!( stderr.contains("ERROR[no-panic]: detected panic in function `slice_access`"));
-
+        */
         Ok(())
     }
 }
