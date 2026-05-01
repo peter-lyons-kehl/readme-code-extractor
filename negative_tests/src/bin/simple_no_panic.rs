50adc55428c750e662a0c3073bcdb64216cb95d7
Peter Lyons Kehl <peter.kehl@gmail.com>
2026-04-30 21:31:36 -0700

    Tests

--- negative_tests/src/bin/simple_no_panic.rs
+++ negative_tests/src/bin/simple_no_panic.rs
@@ -1,6 +1,6 @@
 readme_code_extractor::all!(
     r##"
-    file_path = "simple_no_panic.md"
+    markdown_file_path = "simple_no_panic.md"
     start_prefix = "#[::no_panic::no_panic] fn main() {"
     
     final_suffix = "}"
