a01965ae3f31a2ec534ed442eded60a6cab6cc80
Peter Lyons Kehl <peter.kehl@gmail.com>
2026-05-02 08:28:26 -0700

    Negative tests

--- negative_tests/src/lib.rs
+++ negative_tests/src/lib.rs
@@ -1,8 +1,9 @@
+// See also ../../negative_tests_runner/src/lib.rs.
 #[cfg(not(debug_assertions))]
 compile_error!(
     "Build in debug mode only. Otherwise, since we have multiple sequential parts \
                 where each fails with panic!, a release build would optimize the subsequent \
-                panics out."
+                panics out. Then the error message(s) would not match."
 );
 
 #[cfg(test)]
--- negative_tests_runner/src/lib.rs
+++ negative_tests_runner/src/lib.rs
@@ -6,14 +6,17 @@ mod tests {
     #[test]
     fn simple_no_panic() -> Result<(), Box<dyn Error>> {
         // Do NOT set arg("--release") if having multiple sequential parts where each fails with
-        // panic!. Otherwise release build will optimize the subsequent panics out! This is also
-        // enforced by ../negative_tests/src/lib.rs
+        // panic!. Otherwise release build will optimize the subsequent panics out! Then the error
+        // message(s) would not match.
+        //
+        // That is also enforced by ../negative_tests/src/lib.rs
         let cmd = Command::new("cargo").arg("build");
         let cmd = cmd.current_dir(std::fs::canonicalize("../negative_tests")?);
 
         let assert = cmd.assert().failure();
 
-        assert.stderr_eq(
+        // Using stderr_eq is less readable than multiple stderr.contains(...)
+        /*assert.stderr_eq(
             "...\n\
              [..]ERROR[no-panic]: detected panic in function `main`[..]\n\
              ...\n\
@@ -21,14 +24,13 @@ mod tests {
              ...\n\
              [..]ERROR[no-panic]: detected panic in function `slice_access`[..]\n\
              ...\n",
-        );
-        /*
+        );*/
         let output = assert.get_output();
-        let stderr: &str = str::from_utf8( &output.stderr )?;
+        let stderr: &str = str::from_utf8(&output.stderr)?;
 
-        assert!( stderr.contains("ERROR[no-panic]: detected panic in function `divide`"));
-        assert!( stderr.contains("ERROR[no-panic]: detected panic in function `slice_access`"));
-        */
+        assert!(stderr.contains("ERROR[no-panic]: detected panic in function `main`"));
+        assert!(stderr.contains("ERROR[no-panic]: detected panic in function `divide`"));
+        assert!(stderr.contains("ERROR[no-panic]: detected panic in function `slice_access`"));
         Ok(())
     }
 }
